a2e63d6d0febd1de1a0b92f58fdcdac676048020
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2024-03-17 15:44:04 +0100

    Create elgoog.js

---
+++ TROIOBI/elgoog.js
@@ -0,0 +1,11 @@
+	 <div id="google_translate_element"></div>
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'ru'}, 'google_translate_element');
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+<p>©hoo$e ЛÄнgიAge©///₾ÄngიAge® Ekohomei©Å TÅLKiNg ი.ბ.м.ლ.</p><p><A HREF="http://geo.rf.gd">geo.rf.gd</A><BR><p/>
